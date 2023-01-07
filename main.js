70ec7eaef7147ed907ee3ed0f7fda4fdd553e151
yadhveer28 <106899427+yadhveer28@users.noreply.github.com>
2023-01-07 22:04:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+ console.log("my first console based project");
+
+ var first_name = "yadhveer";
+ var last_name = "singh";
+
+ var full_name = first_name.concat(last_name);
+ console.log(full_name);
+
+ var a = 10 ;
+ var b = 5 ;
+
+ var add = a+b ;
+ console.log(add);
+
+ var subtract = a-b ;
+ console.log(subtract) ; 
+    
+ var multiply = a*b ;
+ console.log(multiply) ;
+
+ var division = a/b ;
+ console.log(division)
